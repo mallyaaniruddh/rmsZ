459fd3458642ed66e9d81f91cb891ebe09d66a66
mallyaaniruddh <24124133@studentmail.ul.ie>
2025-03-16 19:59:10 +0000

    Fix GitHub Pages deployment - updated vite.config.js and _redirects

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/rms-frontend/",  // this matches GitHub repo name
+  base: "/rmsZ/",  // this matches GitHub repo name
 });
